ea51dbfcaed634af02194af4a98fbe02cfc04f31
tttuer <tttuer1@naver.com>
2025-07-15 10:53:35 +0900

    bug fix

--- database/connection.py
+++ database/connection.py
@@ -3,16 +3,11 @@ from typing import Optional
 
 from pydantic_settings import BaseSettings
 from sqlmodel import Session, create_engine
-
-from routes.pages import settings
-
-# 환경 변수에서 데이터베이스 정보 가져오기
-database_user = os.getenv("DATABASE_USER")
-database_password = os.getenv("DATABASE_PASSWORD")
+from utils.settings import settings
 
 # MySQL 데이터베이스 설정
-database_connection_string = 'mysql+mysqlconnector://test_user:test_user@localhost:3306/baeksung'
-# database_connection_string = f'mysql+mysqlconnector://{settings.db_user}:{settings.db_password}@mysql:3306/baeksung'
+# database_connection_string = 'mysql+mysqlconnector://test_user:test_user@localhost:3306/baeksung'
+database_connection_string = f'mysql+mysqlconnector://{settings.db_user}:{settings.db_password}@localhost:3306/baeksung'
 # database_connection_string = f'mysql+mysqlconnector://{database_user}:{database_password}@localhost:3306/pyeongtaek'
 engine_url = create_engine(database_connection_string, echo=True)
 
--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from routes.bus_schedules import schedule_router
 from routes.captcha_route import captcha_router
 from routes.ddocks import ddock_router
 from routes.notices import notice_router
-from routes.pages import page_router
 from routes.qas import qa_router
 from routes.users import users_router
 from utils.settings import settings
@@ -52,7 +51,6 @@ api_router.include_router(captcha_router)
 # api_router를 메인 애플리케이션에 추가
 app.include_router(api_router)
 app.include_router(captcha_router)
-app.include_router(page_router)  # 프론트엔드 라우터는 /api가 필요 없으므로 그대로 추가
 
 # 프론트 세팅
 # 정적 파일 경로 설정
